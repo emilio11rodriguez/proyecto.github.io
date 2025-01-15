52cd91f3b7b9345577c81fb9609f466f8023d6c1
emilio11rodriguez <emilio.rodriguez@ucuenca.edu.ec>
2025-01-15 15:47:59 -0500

    conexion nueva

--- proyecto cloud/my-app/conexion/conexionBD.py
+++ proyecto cloud/my-app/conexion/conexionBD.py
@@ -9,11 +9,11 @@ def connectionBD():
     try:
         # connection = mysql.connector.connect(
         connection = mysql.connector.connect(
-            host="junction.proxy.rlwy.net",
+            host="autorack.proxy.rlwy.net",
                 #host="viaduct.proxy.rlwy.net",
-            port=47711,
+            port=16815,
             user="root",
-            passwd="KKUxiDQtzZVeiKiVDMDMSGBhyyAxMfXh",
+            passwd="UIEDmDTXjLoTkQRoNmmpbIIXuSUebGiy",
                 #passwd="-D2eD6aDb5Bg6dEbhAAeBB6gd3EheaBg",
             database="railway",
                 #database="crud_python",
